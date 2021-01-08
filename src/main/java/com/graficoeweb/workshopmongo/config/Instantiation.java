5d3df3a8bf9993b083239054e8a1a3152155bb79
pesdesigner <pauloeduardo.dg@outlook.com>
2021-01-07 22:56:15 -0300

    Acrescentando comentarios aos Posts

--- src/main/java/com/graficoeweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/graficoeweb/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.graficoeweb.workshopmongo.domain.Post;
 import com.graficoeweb.workshopmongo.domain.User;
 import com.graficoeweb.workshopmongo.dto.AuthorDTO;
+import com.graficoeweb.workshopmongo.dto.CommentDTO;
 import com.graficoeweb.workshopmongo.repository.PostRepository;
 import com.graficoeweb.workshopmongo.repository.UserRepository;
 
@@ -40,10 +41,19 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("22/03/2020"), "Bom dia", "A viagem foi incrível!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem amigo!", sdf.parse("21/03/2020"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2020"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 	
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		
 	}
 }
