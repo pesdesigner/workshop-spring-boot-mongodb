c8c4246e06b8cc869badbf194fa95a238cf0b367
pesdesigner <pauloeduardo.dg@outlook.com>
2021-01-07 22:25:56 -0300

    Referenciando os Posts do usuário

--- src/main/java/com/graficoeweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/graficoeweb/workshopmongo/config/Instantiation.java
@@ -42,5 +42,8 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("22/03/2020"), "Bom dia", "A viagem foi incrível!", new AuthorDTO(maria));
 	
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
