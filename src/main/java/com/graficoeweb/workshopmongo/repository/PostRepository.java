9c63ddedd6560c99fe5c2391dfe09202de111a20
pesdesigner <pauloeduardo.dg@outlook.com>
2021-01-11 23:06:38 -0300

    Consulta simples com query methods

--- src/main/java/com/graficoeweb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/graficoeweb/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.graficoeweb.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.graficoeweb.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
 
